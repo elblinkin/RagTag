1801483a2e8a3d247d94a1ed6647135f795ddfaf
Laura Beth Lincoln <llincoln@etsy.com>
2011-10-19 15:27:41 -0400

    Complete the first implementation of the Couch_Store_Factory

--- phplib/Taggle/Couch/Store/Factory.php
+++ phplib/Taggle/Couch/Store/Factory.php
@@ -4,5 +4,7 @@ class Couch_Store_Factory implements Store_Factory {
 
     function create() {
         $couch = new CouchdbClient("http://localhost:5984", false, "taggle");
+        $store = new CouchStore($couch);
+        return $store;
     }
 }
