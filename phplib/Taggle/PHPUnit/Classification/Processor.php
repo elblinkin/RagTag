b2690cce623563490bcac1f5924b7632a7c9b12b
Laura Beth Lincoln <llincoln@etsy.com>
2011-10-20 16:39:29 -0400

    Implemented the PHPUnit logging.

--- phplib/Taggle/PHPUnit/Classification/Processor.php
+++ phplib/Taggle/PHPUnit/Classification/Processor.php
@@ -2,7 +2,22 @@
 
 class Taggle_PHPUnit_Classification_Processor implements Taggle_Document_Processor {
 
-    function process($document, $ref_id=null) {
-        // TODO
+    private $store;
+
+    function __construct(Taggle_Store $store) {
+        $this->store = $store;
+    }
+
+    function process($filename, $ref_id=null) {
+        $documents = array();
+        $contents = file_get_contents($filename);
+        $json = '[' . preg_replace("/}\n?{/", '},{', $contents) .']';
+        $messages = json_decode($json);
+        foreach ($messages as $message) {
+            $message->taggle_type = 'phpunit_classification';
+            $message->ref_id = $ref_id;
+            $documents[] = $message;
+        }
+        return $this->store->batchSave($documents);
     }
 }
