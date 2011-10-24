29f45cc8ff3b05d8608dbc773e3ed2f673f3659a
Laura Beth Lincoln <llincoln@etsy.com>
2011-10-24 17:19:30 -0400

    Update the info log processor to parse out files and line referenced in PHP NOTICEs

--- phplib/Taggle/PHP/InfoLog/Processor.php
+++ phplib/Taggle/PHP/InfoLog/Processor.php
@@ -35,19 +35,32 @@ class Taggle_PHP_InfoLog_Processor implements Taggle_Document_Processor {
                 $document->log_id = $matches[self::LOG_ID];
                 $document->log_level = 'debug';
                 $document->log_namespace = $matches[self::LOG_NAMESPACE];
-            
+                
                 $file_line = explode(':', $matches[self::FILE_LINE]);
                 $document->source = new StdClass;
                 $document->source->file = $file_line[0];
                 $document->source->line = $file_line[1];
                 
                 $document->log_message = $matches[self::DEBUG_MESSAGE];
+            
+                if ($document->log_namespace === 'DEV_DEBUG') {
+                    if (preg_match_all('@(/[^\\s]+\.php) (on line|line) ([0-9]+)@', $document->log_message, $matches)) {
+                        $document->references = array();
+                        $reference = new StdClass;
+                        foreach ($matches[1] as $k => $filename) {
+                            $reference->file = $filename;
+                            $document->references[$k] = $reference;
+                        }
+                        foreach ($matches[3] as $k => $line_number) {
+                            $document->references[$k]->line = $line_number;
+                        }
+                    }
+                }
             } else if (preg_match(self::INFO_REGEX, $line, $matches)) {
                 $document->log_date = $matches[self::LOG_DATE];
                 $document->log_id = $matches[self::LOG_ID];
                 $document->log_level = 'info';
                 $document->log_namespace = $matches[self::LOG_NAMESPACE];
-                
                 $document->log_message = $matches[self::INFO_MESSAGE];
             }
             $documents[] = $document;
