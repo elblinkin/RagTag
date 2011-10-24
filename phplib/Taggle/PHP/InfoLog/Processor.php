005694a5f38b586824f4623122bf43944af1ea14
Laura Beth Lincoln <llincoln@etsy.com>
2011-10-24 16:47:09 -0400

    Inital version of the info log processor.
    Git information should be indexed after these logs are added.
    
    We can #1 have a text highlighter that adds git blame info to the UI, and
    then #2 look at adding the per person stuff in a post process.

--- phplib/Taggle/PHP/InfoLog/Processor.php
+++ phplib/Taggle/PHP/InfoLog/Processor.php
@@ -2,7 +2,57 @@
 
 class Taggle_PHP_InfoLog_Processor implements Taggle_Document_Processor {
 
+    const DEBUG_REGEX = '@\[(.*)\] \[(.*)\] \[debug\] \[(.*)\] \[(.*)\] \[(.*)\] (.*)@';
+    const INFO_REGEX = '@\[(.*)\] \[(.*)\] \[info\] \[(.*)\] \[(.*)\] (.*)@';
+    
+    const LOG_DATE = 1;
+    const LOG_ID = 2;
+    const LOG_NAMESPACE = 3;
+    const FILE_LINE = 4;
+    
+    const DEBUG_MESSAGE = 6;
+    const INFO_MESSAGE = 5;
+
+    private $store;
+
+    function __construct(
+        Taggle_Store $store
+    ) {
+        $this->store = $store;
+    }
+    
     function process($document, $ref_id=null) {
-        // TODO
+        $documents = array();
+        $handle = fopen($document, 'r');
+        while(($line = fgets($handle, 4096 * 3)) !== false) {
+            $document = new StdClass;
+            $document->raw = $line;
+            $document->ref_id = $ref_id;
+            $document->taggle_type = 'php_infolog';
+            
+            if (preg_match(self::DEBUG_REGEX, $line, $matches)) {
+                $document->log_date = $matches[self::LOG_DATE];
+                $document->log_id = $matches[self::LOG_ID];
+                $document->log_level = 'debug';
+                $document->log_namespace = $matches[self::LOG_NAMESPACE];
+            
+                $file_line = explode(':', $matches[self::FILE_LINE]);
+                $document->source = new StdClass;
+                $document->source->file = $file_line[0];
+                $document->source->line = $file_line[1];
+                
+                $document->log_message = $matches[self::DEBUG_MESSAGE];
+            } else if (preg_match(self::INFO_REGEX, $line, $matches)) {
+                $document->log_date = $matches[self::LOG_DATE];
+                $document->log_id = $matches[self::LOG_ID];
+                $document->log_level = 'info';
+                $document->log_namespace = $matches[self::LOG_NAMESPACE];
+                
+                $document->log_message = $matches[self::INFO_MESSAGE];
+            }
+            $documents[] = $document;
+        }
+        fclose($handle);
+        return $this->store->batchSave($documents);
     }
 }
