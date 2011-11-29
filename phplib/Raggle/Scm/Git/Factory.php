5b0b32e290bac6bd0819b5e9d72472871b8bfe01
Laura Beth Lincoln <llincoln@etsy.com>
2011-11-29 15:50:16 -0500

    Add a factory for creating the git scm.

---
+++ phplib/Raggle/Scm/Git/Factory.php
@@ -0,0 +1,19 @@
+<?php
+
+class Raggle_Scm_Git_Factory {
+
+    public function create(
+        $root_dir,
+        Raggle_Exec $exec,
+        Raggle_Logger $logger
+    ) {
+        return new Raggle_Scm_Git(
+            new Raggle_Scm_Git_Action_Clean($root_dir, $exec),
+            new Raggle_Scm_Git_Action_Clone($root_dir, $exec),
+            new Raggle_Scm_Git_Action_Exists($root_dir, $logger),
+            new Raggle_Scm_Git_Action_Fetch($root_dir, $exec),
+            new Raggle_Scm_Git_Action_Validate($root_dir, $exec, $logger),
+            $logger
+        );   
+    }
+}
