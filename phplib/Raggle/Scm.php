76779476e17d722d24be84e78b3ed488a12187c5
Laura Beth Lincoln <llincoln@etsy.com>
2011-12-05 15:29:33 -0500

    Add comments to the SCM interface so I do not forget all the dynamic abuses.

--- phplib/Raggle/Scm.php
+++ phplib/Raggle/Scm.php
@@ -2,11 +2,27 @@
 
 interface Raggle_Scm {
 
+    /**
+     * @return the name for this SCM
+     */
     function getName();
     
+    /**
+     * @param Raggle_Scm_Repository $repo the repository to checkout
+     */
     function checkout($repo);
     
+    /**
+     * @param Raggle_Scm_Repository $repo the repository to get the HEAD revision for
+     * @return string the HEAD revision string
+     */
     function getRevision($repo);
     
-    function getChangeLog($repo, $start_rev, $end_rev);
+    /**
+     * @param Raggle_Scm_Repository $repo the repository to get the changelog for
+     * @param string|null [$start_revision] the optional start revision
+     * @param string|null [$end_revision] the option end revision
+     * @return string|array either the file name for the log or the log as an array
+     */
+    function getChangeLog($repo, $start_revision = null, $end_revision = null);
 }
