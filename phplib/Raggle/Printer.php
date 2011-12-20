5a6a8d92df5940e35a050f5b5571e2f62b7ffac5
Laura Beth Lincoln <llincoln@etsy.com>
2011-12-20 13:36:50 -0500

    Add a phpdoc comment to this interface.

--- phplib/Raggle/Printer.php
+++ phplib/Raggle/Printer.php
@@ -2,5 +2,10 @@
 
 interface Raggle_Printer {
 
+    /**
+     * Writes the message to whatever the underlying logging mechanism is.
+     *
+     * @param string $message message to write out to the underlying mechanism.
+     */
     function write($message);
 }
