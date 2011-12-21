bdd10d48f487a8694651ab95c0fc7439f0c89cb1
Laura Beth Lincoln <llincoln@etsy.com>
2011-12-21 10:38:19 -0500

    Add a test for the scm manager.

--- phplib/Raggle/Scm/Repository.php
+++ phplib/Raggle/Scm/Repository.php
@@ -2,5 +2,10 @@
 
 interface Raggle_Scm_Repository {
 
+    /**
+     * Returns the name of the repository.
+     *
+     * @return the name of the respository.
+     */
     function getName();
 }
---
+++ tests/phpunit/Raggle/Scm/ManagerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class Raggle_Scm_ManagerTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @expectedException PHPUnit_Framework_Error
+     * @expectedExceptionMessage Undefined index: git
+     */
+    function testGetScm_doesNotExist() {
+        $manager = new Raggle_Scm_Manager(array());
+        $manager->getScm('git');
+    }
+    
+    function testGetScm() {
+        $git = $this->getMockBuilder('Raggle_Scm_Git');
+
+        $manager = new Raggle_Scm_Manager(array('git' => $git));
+        $this->assertEquals($git, $manager->getScm('git'));
+    }
+}
